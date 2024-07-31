dc960a3e3a2fe6d251bbb4fe9fc896a70460fe35
Jeremy Simmons <101369930+novaTehnika@users.noreply.github.com>
2024-07-31 12:36:12 -0500

    add intake filter to analysis and other adjustments to system design

--- study_lowBranchDesign.m
+++ study_lowBranchDesign.m
@@ -53,7 +53,8 @@ par.p_o = 101.3e3; % [Pa]  Atmospheric pressure (reference)
 par.gamma = 1.4; % [-]ratio of specific heats for air
 
 % WEC-driven pump
-D = 0.23/2; % pump displacement
+D = 0.23; % pump displacement
+h_pump = 5; % [m] water depth of pump
 
  % intake check valve of WEC-driven pump
 kv_cv = @(ID) 0.0084766*ID^2+0.0000251036*ID; % [m^3/s/Pa^0.5 = f(m)] check valve flow coeff. as a function of nominal pipe ID
@@ -62,18 +63,49 @@ dp_cv = 2*pc_cv;
 
 % Piping
  % intermediate line between WEC-driven pump port and check valve
-L_intermediate = 1; % [m]
+L_intermediate = 5; % [m]
  % intake pipeline
-L_intake = 0; % [m]
+L_intake = 5; % [m]
+
+% Intake filter
+N_housing = 1;
+ % Shelco High Flow Multi-Cartridge Housings (3, 4, 7, or 12 cartridges)
+R_6F = 0.00450839456109*6894.757*15850.3; % [(psi/gpm) -> Pa/(m^3/s)]
+R_8F = 0.001718715182745*6894.757*15850.3; % [(psi/gpm) -> Pa/(m^3/s)]
+R_10F = 0.001170833178906*6894.757*15850.3; % [(psi/gpm) -> Pa/(m^3/s)]
+R_12F = 0.000673426200783*6894.757*15850.3; % [(psi/gpm) -> Pa/(m^3/s)]
+
+ % Filter
+N_cart = 12;
+  % 5um filters, 60"
+flow_ShelcoHFC5u60in = [0
+102.857142857143
+202.285714285714
+300
+399.428571428571
+497.142857142857
+589.714285714285]/15850.3; % [(gpm) -> m^3/s]
+
+pdiff_ShelcoHFC5u60in = [0
+0.264350453172205
+0.876132930513595
+1.63141993957704
+2.59818731117825
+3.74622356495468
+5]*6894.757; % [(psi) -> Pa]
 
 % Minor losses in the circuit
  % Long radius, 90-degree bends with flanged fittings
 k_90degLongRadFlanged = 0.2; % loss coeff.
-n_90degLongRadFlanged = 2; % number of elements of this type
+n_90degLongRadFlanged = 1; % number of elements of this type
 
  % Tee fitting with flow making a 90-degree turn (branched flow)
 k_teeBranchedFlow = 1.0;
 n_teeBranchedFlow = 1;
+
+% Abrupt discharge into reservoir
+k_dischargeSharp = 1.0;
+n_dischargeSharp = 1;
 
  % Intake modeled as flow from a resivoiur into a pipe with a well radiused
  % transition to the nominal pipe diameter
@@ -107,28 +139,45 @@ for i = 1:ni
     dq_dt = D*theta_ddot_dom(i);
     for j = 1:nj
         A_pipe = pi/4*d_pipe(j)^2;
-        
+        v_pipe(i,j) = q/A_pipe;
+        % Major losses
         deltaP_major(i,j) = (flowR(q,d_pipe(j),L_intermediate,par) + ...
                              flowR(q,d_pipe(j),L_intake,par))*q;
         
+        % Minor losses, piping
         deltaP_minor(i,j) = k_total*par.rho/2*(q/A_pipe)^2;
         
+        % Intake filter
+        dp_filter(i,j) = interp1(flow_ShelcoHFC5u60in, ...
+                                 pdiff_ShelcoHFC5u60in, ...
+                                 q/(N_housing*N_cart),'spline');
+        dp_housing(i,j) = R_12F*q/N_housing;
+        dp_totalFilter(i,j) = dp_filter(i,j) + dp_housing(i,j);
+
+        % Check valves
         kv = kv_cv(d_pipe(j));
         deltaP_cv(i,j) = max(dp_cv/(kv*sqrt(pc_cv+dp_cv))*q+pc_cv, ...
                              (q/kv)^2);
 
-
+        % Acceleration head
         I_intermediate = par.rho*L_intermediate/A_pipe;
         I_intake = par.rho*L_intake/A_pipe;
         deltaP_accel(i,j) = (I_intermediate+I_intake)*dq_dt;
 
+        % Total pressure drop
         deltaP_total(i,j) = deltaP_major(i,j) + deltaP_minor(i,j) + ...
-                            deltaP_cv(i,j) + deltaP_accel(i,j);
+                            deltaP_cv(i,j) + deltaP_accel(i,j) + ...
+                            dp_totalFilter(i,j);
 
+        % cavitation index
+         % approx. orifice area of CV (assumes discharge coeff. of
+         % 0.6)
         A_orifice = kv/0.6*sqrt(par.rho/2);
-        p_d = par.p_o - deltaP_total(i,j) ...
-            + k_90degLongRadFlanged*par.rho/2*(q/A_pipe)^2;
-        cavIndex(i,j) = (p_d - par.p_vap)/(1/2*par.rho*((q/A_orifice)^2));
+         % add back minor loss of discharge into cylinder
+        p_d = (par.p_o + par.rho*9.81*h_pump) - deltaP_total(i,j) ...
+            + k_dischargeSharp*par.rho/2*(q/A_pipe)^2;
+         % caviation index
+        cavIndex(i,j) = (p_d - par.p_vap)/(1/2*par.rho*(q/A_orifice)^2);
 
     end
 end
@@ -181,4 +230,12 @@ figure
 plot(d_pipe*100,min_cavIndex)
 xlabel('pipe diameter (cm)')
 ylabel('cavitation index')
-title('Maximum Total Pressure Drop')
+title('Minimum Cavitation Index')
+
+%% Mean flow velocity
+max_v_pipe = max(v_pipe,[],1);
+figure
+plot(d_pipe*100,max_v_pipe)
+xlabel('pipe diameter (cm)')
+ylabel('velocity (m/s)')
+title('Mean Flow Velocity')
